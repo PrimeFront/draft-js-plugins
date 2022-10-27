0c64abb663f9be92e47a8ca38de6b185c00714be
Adam Szwelicki <adashizu@poczta.fm>
2022-10-27 12:41:00 +0200

    Revert "Test of lib add"
    This reverts commit 0a72c0c04a63df4a1e6eb0309f96342707d86e39.

--- packages/mention/lib/mentionStrategy.d.ts
+++
@@ -1,3 +0,0 @@
-import { ContentBlock, ContentState } from 'draft-js';
-declare const findMentionEntities: (triggers: string[]) => (contentBlock: ContentBlock, callback: (start: number, end: number) => void, contentState: ContentState) => void;
-export default findMentionEntities;
--- packages/mention/lib/modifiers/addMention.d.ts
+++
@@ -1,3 +0,0 @@
-import { EditorState } from 'draft-js';
-import { MentionData } from '..';
-export default function addMention(editorState: EditorState, mention: MentionData, mentionPrefix: string, mentionTrigger: string, entityMutability: 'SEGMENTED' | 'IMMUTABLE' | 'MUTABLE'): EditorState;
