0a72c0c04a63df4a1e6eb0309f96342707d86e39
Adam Szwelicki <adashizu@poczta.fm>
2022-10-27 12:06:10 +0200

    Test of lib add

---
+++ packages/mention/lib/utils/defaultSuggestionsFilter.d.ts
@@ -0,0 +1,3 @@
+import { MentionData, MultiMentionData } from '..';
+declare const defaultSuggestionsFilter: (searchValue: string, suggestions: MultiMentionData | MentionData[], trigger?: string) => MentionData[];
+export default defaultSuggestionsFilter;
